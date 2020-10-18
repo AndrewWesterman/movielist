6668e0e69543d7424c43a0b6ff927d12b0c0c39a
Andrew Westerman <awesterman1507@gmail.com>
2020-10-18 13:59:14 -0700

    Added create and get all functionality

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../../models/Movie');
+
 // @route   {GET} /api/movies
 // @desc    Gets all movies
 router.get('/', async (req, res) => {
-    res.send('/movies');
+    try {
+        let movies = await Movie.find({});
+        console.log(movies);
+        return res.json(movies);
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send('Server Error');
+    }
 });
 
 // @route   {GET} /api/movies/:id
@@ -13,7 +22,18 @@ router.get('/:id', async (req, res) => {});
 
 // @route   {POST} /api/movies
 // @desc    Adds movie to system
-router.post('/', async (req, res) => {});
+router.post('/', async (req, res) => {
+    try {
+        const newMovie = new Movie({
+            ...req.body,
+        });
+        const movie = await newMovie.save();
+        return res.json(movie);
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send('Server Error');
+    }
+});
 
 // @route   {PUT} /api/movies/:id
 // @desc    Updates movie with :id in system
