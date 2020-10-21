5a2ba974a7d93b3370f4614ac610beb022a00ad0
Andrew Westerman <awesterman1507@gmail.com>
2020-10-20 20:30:35 -0700

    Make it look nice with bootstrap

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { MoviesComponent } from './components/movies/movies.component';
 import { MovieFormComponent } from './components/movie-form/movie-form.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, MoviesComponent, MovieFormComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
