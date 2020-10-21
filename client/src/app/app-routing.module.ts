d2b56383d774afaa7b016862533ef28df9c77cb4
Andrew Westerman <awesterman1507@gmail.com>
2020-10-20 17:25:57 -0700

    Configured routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieFormComponent } from './components/movie-form/movie-form.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/movies', pathMatch: 'full' },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'add-movie', component: MovieFormComponent },
+  { path: 'edit-movie/:id', component: MovieFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
